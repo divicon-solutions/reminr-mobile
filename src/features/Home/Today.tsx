49ce44ec74c4c957985126c4bc65a9dabd891301
kalyanbabutangella <tkalyanbabu63@gmail.com>
2024-02-20 16:10:36 -0500

    Today Screen and View Reminder screen

--- src/features/Home/Today.tsx
+++ src/features/Home/Today.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, RefreshControl, Modal } from "react-native";
+import { View, FlatList, RefreshControl, Modal } from "react-native";
 import React, { Fragment } from "react";
 import { BottomTabNavigationProps } from "@navigations/types";
 import { ReminderDto, useRemindersControllerFindAll } from "@api";
@@ -6,6 +6,9 @@ import Loader from "@components/Loader";
 import { TouchableOpacity } from "react-native";
 import ViewReminder from "@features/Medication/ViewReminder";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { Card, Text } from "react-native-paper";
+import moment from "moment";
+import MCIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 type TodayProps = BottomTabNavigationProps<"Today">;
 
@@ -21,40 +24,63 @@ export default function Today({ navigation }: TodayProps) {
 	if (isLoading) return <Loader />;
 	return (
 		<Fragment>
-			<FlatList
-				data={data}
-				renderItem={({ item }) => (
-					<TouchableOpacity onPress={() => handleReminderPress(item)}>
-						<View>
-							<Text>{item.title}</Text>
-						</View>
-					</TouchableOpacity>
-				)}
-				keyExtractor={(item) => item.id.toString()}
-				ListEmptyComponent={<Text>No Medications</Text>}
-				ListHeaderComponent={<Text>Today</Text>}
-				refreshControl={<RefreshControl refreshing={isRefetching} onRefresh={refetch} />}
-			/>
-
+			<View style={{ padding: 16 }}>
+				<Text style={{ fontWeight: "700", fontSize: 20, marginBottom: 10 }}>Medications Due:</Text>
+				<FlatList
+					data={data}
+					renderItem={({ item }) => (
+						<TouchableOpacity onPress={() => handleReminderPress(item)}>
+							<View style={{ flex: 1 }}>
+								<Card
+									mode="contained"
+									style={{ backgroundColor: "white", borderRadius: 5, marginBottom: 10 }}
+								>
+									<Card.Content style={{ flexDirection: "row", justifyContent: "space-between" }}>
+										<View>
+											<Text variant="bodyMedium" style={{ fontSize: 18, fontWeight: "bold" }}>
+												{item.title}
+											</Text>
+											<Text>2 pills</Text>
+										</View>
+										<View>
+											<MCIcon name="timer-sand-complete" size={24} color="#f09337" />
+										</View>
+									</Card.Content>
+									<Card.Content>
+										<Text style={{ marginTop: 15, fontSize: 12 }}>
+											{moment(item.remindAt, "hh:mmA").format("hh:mm A")}
+										</Text>
+									</Card.Content>
+								</Card>
+							</View>
+						</TouchableOpacity>
+					)}
+					keyExtractor={(item) => item.id.toString()}
+					ListEmptyComponent={<Text>No Medications</Text>}
+					refreshControl={<RefreshControl refreshing={isRefetching} onRefresh={refetch} />}
+				/>
+			</View>
 			<Modal animationType="slide" presentationStyle="pageSheet" visible={modalVisible}>
-				<View style={{ flexDirection: "row", justifyContent: "flex-start" }}>
-					<TouchableOpacity
+				<View style={{ backgroundColor: "#eeeeee", flex: 1 }}>
+					<View style={{ flexDirection: "row", justifyContent: "flex-start" }}>
+						<TouchableOpacity
+							onPress={() => {
+								setModalVisible(!modalVisible);
+							}}
+						>
+							<Icon name="close" size={24} />
+						</TouchableOpacity>
+					</View>
+					{selectedReminder && <ViewReminder reminder={selectedReminder} />}
+
+					{/* <TouchableOpacity
 						onPress={() => {
 							setModalVisible(!modalVisible);
 						}}
 					>
-						<Icon name="close" size={24} />
-					</TouchableOpacity>
+						<Text>Hide Modal</Text>
+					</TouchableOpacity> */}
 				</View>
-				<View>{selectedReminder && <ViewReminder reminder={selectedReminder} />}</View>
-
-				<TouchableOpacity
-					onPress={() => {
-						setModalVisible(!modalVisible);
-					}}
-				>
-					<Text>Hide Modal</Text>
-				</TouchableOpacity>
 			</Modal>
 		</Fragment>
 	);
--- src/features/Medication/ViewReminder.tsx
+++ src/features/Medication/ViewReminder.tsx
@@ -1,7 +1,10 @@
-import { View, Text } from "react-native";
+import { Dimensions, View } from "react-native";
 import React from "react";
 import { ReminderDto } from "@api";
-import { Button } from "react-native-paper";
+import { Button, Card, Text } from "react-native-paper";
+import moment from "moment";
+import { SafeAreaView } from "react-native-safe-area-context";
+import KeyboardAvoidView from "@components/KeyboardAvoidView";
 
 type ViewReminderProps = {
 	reminder: ReminderDto;
@@ -9,15 +12,65 @@ type ViewReminderProps = {
 
 export default function ViewReminder(props: ViewReminderProps) {
 	const { reminder } = props;
-	return (
-		<View>
-			<Text>{reminder.title}</Text>
-			<Text>Dose: {reminder.description}</Text>
-			<Text>Time: {reminder.remindAt}</Text>
 
-			<Button onPress={() => {}}>Snooze</Button>
-			<Button onPress={() => {}}>Skip</Button>
-			<Button onPress={() => {}}>Take Medication</Button>
-		</View>
+	const renderCard = (title: string, content: string) => (
+		<Card mode="contained" style={{ backgroundColor: "white", borderRadius: 5, marginBottom: 20 }}>
+			<Card.Content>
+				{title === "medicationName" ? (
+					<Text variant="bodyMedium" style={{ fontSize: 18, fontWeight: "bold" }}>
+						{content}
+					</Text>
+				) : (
+					<View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+						<Text>{title ? `${title}:` : ""}</Text>
+						<Text>{content}</Text>
+					</View>
+				)}
+			</Card.Content>
+		</Card>
+	);
+
+	return (
+		<KeyboardAvoidView
+			style={{ padding: 20 }}
+			contentContainerStyle={{ flexGrow: 1, justifyContent: "space-between" }}
+		>
+			<View>
+				{renderCard("medicationName", reminder.title)}
+				{renderCard("Dose", "1 pills")}
+				{renderCard("Time", moment(reminder.remindAt, "hh:mmA").format("hh:mm A"))}
+			</View>
+			<SafeAreaView>
+				<Button
+					mode="contained"
+					onPress={() => {}}
+					style={{
+						width: 300,
+						alignSelf: "center",
+						marginBottom: 15,
+						borderRadius: 0,
+						backgroundColor: "#f9af47",
+					}}
+				>
+					<Text style={{ fontWeight: 700 }}>Snooze</Text>
+				</Button>
+				<SafeAreaView style={{ flexDirection: "row", justifyContent: "space-around" }}>
+					<Button
+						mode="outlined"
+						onPress={() => {}}
+						style={{ width: Dimensions.get("window").width / 3, borderRadius: 0 }}
+					>
+						Skip
+					</Button>
+					<Button
+						mode="contained"
+						onPress={() => {}}
+						style={{ width: Dimensions.get("window").width / 3, borderRadius: 0 }}
+					>
+						Taken
+					</Button>
+				</SafeAreaView>
+			</SafeAreaView>
+		</KeyboardAvoidView>
 	);
 }
